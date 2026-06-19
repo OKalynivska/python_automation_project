e5ab58241bf9eec65518530eb10c1919e10f44ed
oksanakalynivska <oksana.kalynivska@upwind.io>
2026-06-19 17:23:30 +0300

    Add LoginPage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,11 @@
+import allure
 from playwright.sync_api import Page
 
 
 class BasePage:
     def __init__(self, page: Page):
         self.page = page
+
+    @allure.step("Navigate to page '{url}'")
+    def navigate(self, url: str):
+        self.page.goto(url)
