aeecb9a4b36d08f47eb33061da5706ba79690999
Oksana <o.kalunivska@gmail.com>
2026-07-02 11:29:32 +0300

    Add test View product page

--- web/pages/view_product_page.py
+++ web/pages/view_product_page.py
@@ -1,6 +1,7 @@
 import allure
 from playwright.sync_api import expect
 
+from web.locators.view_product_locators import ViewProductLocators
 from web.pages.base_page import BasePage
 
 
@@ -11,3 +12,17 @@ class ViewProductPage(BasePage):
     @allure.step("Assert redirect 'View Product page'")
     def assert_view_product_page_redirect(self, id):
         expect(self.page).to_have_url(f'https://automationexercise.com/product_details/{id}'), "Url is not correct"
+
+    @allure.step("Check 'View Product page' structure")
+    def assert_view_product_page_structure(self):
+        expect(self.page.locator(ViewProductLocators.IMAGE)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.PRODUCT_INFO_BLOCK)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.PRODUCT_QUANTITY)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.PRODUCT_INFO_TITLE)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.ADD_TO_CART_BUTTON)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.EMAIL_INPUT_FIELD)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.NAME_INPUT_FIELD)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.REVIEW_TEXTAREA_FIELD)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.PRODUCT_INFO_CATEGORIES)).to_contain_text(["Category", "Availability", "Condition", "Brand"])
+        expect(self.page.locator(ViewProductLocators.SHOP_DETAILS_TAB)).to_be_visible()
+        expect(self.page.locator(ViewProductLocators.SUBMIT_BUTTON)).to_be_visible()
--- web/tests/test_view_product_page.py
+++ web/tests/test_view_product_page.py
@@ -4,5 +4,6 @@ import allure
 class TestViewProductPage:
 
    @allure.title("Check ' View Product page' structure")
-   def test_view_product_page_structure(self):
-      pass
+   def test_view_product_page_structure(self, home_page, view_product_page):
+      home_page.click_on_view_product(2)
+      view_product_page.assert_view_product_page_structure()
